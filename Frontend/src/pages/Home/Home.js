46817bb36900d1b05f7c68f41f3ac9d9f27f5d13
Axel Ribeiro <ribeiro.axel.fabien@gmail.com>
2022-08-29 13:17:13 +0200

    fonction de suppression de post

--- Frontend/src/pages/Home/Home.js
+++ Frontend/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import Header from "../../components/Header/Header";
 import "./Home.css";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -13,6 +14,13 @@ function Home() {
       });
   }, []);
 
+  const handleRemove = (id) => {
+    axios.delete(`http://localhost:3001/api/posts/${id}`).then(() => {
+      const newList = posts.filter((item) => item.id !== id);
+      setPosts(newList);
+    });
+  };
+
   return (
     <div>
       <Header />
@@ -24,9 +32,17 @@ function Home() {
           {posts.map((post) => {
             return (
               <li key={post.id} className="nav_li">
-                {post.title}
-                <i className="fa-regular fa-thumbs-up"></i>
-                <i className="fa-regular fa-thumbs-down"></i>
+                {post.title} : {post.description}
+                <div className="icons_container">
+                  <i className="fa-regular fa-thumbs-up"></i>
+                  <i className="fa-regular fa-thumbs-down"></i>
+                  <button
+                    className="delete_btn"
+                    onClick={() => handleRemove(post.id)}
+                  >
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </div>
               </li>
             );
           })}
